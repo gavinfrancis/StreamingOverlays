00d8dd02ff413455de25654720bf3eaab284b5bf
Gavin Francis <gavinfrancis1312@gmail.com>
2022-09-13 23:07:28 -0600

    Skeleton Project Example

---
+++ ShinyHunting/ShinyHunting/LayoutHelper.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace ShinyHunting
+{
+    class LayoutHelper
+    {
+        private Dictionary<string, string> LaunchSettings = new Dictionary<string, string>();
+        private Dictionary<string, string> DoubleHuntSettings = new Dictionary<string, string>();
+        private Dictionary<string, string> GraphicSettings = new Dictionary<string, string>();
+
+        // Constructor
+        public LayoutHelper()
+        {
+            this.ReadLaunchFileSettings();
+            this.AssignDoubleHuntSettings();
+            this.AssignGraphicSettings();
+        }
+
+        // Switch to dictionary at some point
+        public void ReadLaunchFileSettings()
+        {
+            // Read entire text file content in one string  
+            //string path = Directory.GetCurrentDirectory();
+            string path = "../../../LaunchSettings.txt";
+            this.LaunchSettings = File.ReadAllLines(path).Select(x => x.Split(" = ")).ToDictionary(x => x[0], x => x[1]);
+
+        }
+
+        public void AssignGraphicSettings()
+        {
+            this.GraphicSettings.Add("BorderType", this.LaunchSettings["BorderType"]);
+        }
+
+        public void AssignDoubleHuntSettings()
+        {
+            // Assigns values for screen height and width
+            double screenHeight = Convert.ToDouble(this.LaunchSettings["ScreenHeight"]);
+            double screenWidth = Convert.ToDouble(this.LaunchSettings["ScreenWidth"]);
+
+            // Assigns values for display borders
+            double verticalBorderHeight = screenHeight / 2;
+            double verticalBorderWidth = 15 / 1;
+            this.DoubleHuntSettings.Add("VerticalBorderHeight", verticalBorderHeight.ToString());
+            this.DoubleHuntSettings.Add("VerticalBorderWidth", verticalBorderWidth.ToString());
+
+            double horizontalBorderHeight = 15 / 1;
+            double horizontalBorderWidth = screenWidth / 2;
+            this.DoubleHuntSettings.Add("HorizontalBorderHeight", horizontalBorderHeight.ToString());
+            this.DoubleHuntSettings.Add("HorizontalBorderWidth", horizontalBorderWidth.ToString());
+        }
+
+        public Dictionary<string, string> GetGraphicSettings()
+        {
+            return this.GraphicSettings;
+        }
+
+        public Dictionary<string, string> GetLaunchSettings()
+        {
+            return this.LaunchSettings;
+        }
+
+        public Dictionary<string, string> GetDoubleHuntSettings()
+        {
+            return this.DoubleHuntSettings;
+        }
+    }
+}
---
+++ ShinyHunting/ShinyHunting/MainWindow.xaml.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ShinyHunting
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private LayoutHelper LayoutSettings;
+
+        // Constructor
+        public MainWindow()
+        {
+            this.LayoutSettings = new LayoutHelper();
+
+            DataContext = this;
+            InitializeComponent();
+        }
+
+        // ********************************************************** //
+        // Getters that are accessed within the Main Display Window   //
+        // ********************************************************** //
+
+        // Entire Display Heights and Widths
+        public double ScreenHeight { get { return Convert.ToDouble(this.LayoutSettings.GetLaunchSettings()["ScreenHeight"]);} }
+        public double ScreenWidth  { get { return Convert.ToDouble(this.LayoutSettings.GetLaunchSettings()["ScreenWidth"]);} }
+
+        // Border Heights and Widths
+        public double VerticalBorderHeight   { get { return Convert.ToDouble(this.LayoutSettings.GetDoubleHuntSettings()["VerticalBorderHeight"]);} }
+        public double VerticalBorderWidth    { get { return Convert.ToDouble(this.LayoutSettings.GetDoubleHuntSettings()["VerticalBorderWidth"]);} }
+        public double HorizontalBorderHeight { get { return Convert.ToDouble(this.LayoutSettings.GetDoubleHuntSettings()["HorizontalBorderHeight"]);} }
+        public double HorizontalBorderWidth  { get { return Convert.ToDouble(this.LayoutSettings.GetDoubleHuntSettings()["HorizontalBorderWidth"]);} }
+        
+        // Border Positioning based on X/Y Coordinates
+        // Screen One = Left Screen
+        // Screen Two = Right Screen
+
+        // Positioning for Screen One
+        public double ScreenOne_LeftVerticalBorder_PositionX  { get { return Convert.ToDouble(0);} }
+        public double ScreenOne_LeftVerticalBorder_PositionY { get { return Convert.ToDouble(0); } }
+
+        public double ScreenOne_RightVerticalBorder_PositionX { get { return (this.ScreenWidth / 2) - 10;} }
+        public double ScreenOne_RightVerticalBorder_PositionY { get { return Convert.ToDouble(0); } }
+
+        public double ScreenOne_UpperHorizontalBorder_PositionX { get { return Convert.ToDouble(0); } }
+        public double ScreenOne_UppertHorizontalBorder_PositionY { get { return Convert.ToDouble(0); } }
+
+        public double ScreenOne_LowerHorizontalBorder_PositionX { get { return (0); } }
+        public double ScreenOne_LowerHorizontalBorder_PositionY { get { return this.VerticalBorderHeight; } }
+
+        // Positioning for Screen Two
+        public double ScreenTwo_LeftVerticalBorder_PositionX { get { return (this.ScreenWidth / 2) + 10;} }
+        public double ScreenTwo_LeftVerticalBorder_PositionY { get { return Convert.ToDouble(0);} }
+
+        public double ScreenTwo_RightVerticalBorder_PositionX { get { return this.ScreenWidth;} }
+        public double ScreenTwo_RightVerticalBorder_PositionY { get { return Convert.ToDouble(0);} }
+
+        public double ScreenTwo_UpperHorizontalBorder_PositionX { get { return (this.ScreenWidth / 2) + 10;} }
+        public double ScreenTwo_UppertHorizontalBorder_PositionY { get { return Convert.ToDouble(0);} }
+
+        public double ScreenTwo_LowerHorizontalBorder_PositionX { get { return (this.ScreenWidth / 2) + 10; } }
+        public double ScreenTwo_LowerHorizontalBorder_PositionY { get { return this.VerticalBorderHeight;} }
+
+
+        // Accessing Graphics Paths
+        public string BorderType { get { return this.LayoutSettings.GetGraphicSettings()["BorderType"];} }
+
+
+
+        // Code that allows the Window to be draggable
+        private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+
+        //Below is the boilerplate code supporting PropertyChanged events:
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string name)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(name));
+            }
+        }
+    }
+}
